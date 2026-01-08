687b49edeca6844ca39211d436152f6ef4a32263
laxmibabar011 <laxmibabar011@gmail.com>
2026-01-09 00:21:34 +0530

    Add Docker support

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-expect-error - Next.js types might not be up to date for this new config
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    turbopack: false,
-  },
+  output: "standalone",
+
+
 };
 
 export default nextConfig;
